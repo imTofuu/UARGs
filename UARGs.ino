20205056cf84286dd2fe8579a7f9ef7c0ab79168
tofu <drewbryan08@gmail.com>
2024-03-15 08:04:12 +1300

    test sketch

--- UARGs.ino
+++ UARGs.ino
@@ -1,25 +1,57 @@
 #include "src/ArgDevice.hpp"
+#include <HTTP.h>
 
-ArgDevice device(true);
+ArgDevice device(false);
 
 void setup() {
   device.begin(&Serial2);
+  HTTP::begin("Bryans", "ABCDEFGH");
+
+  Serial.begin(115200);
 
   //device.sendArgs("http arg1 arg2");
 
   device.addEventListener("http", http);
-  device.addEventListener("http2", http);
-  device.addEventListener("http3", http);
-  device.addEventListener("http4", http);
-  Serial.println("startimportant");
-  device.addEventListener("http5", http);
-  Serial.println("deleting now");
-  device.removeEventListener("http", http);
+}
+void loop() {
+  HTTP::update();
+  device.update();
+}
+
+void http(Args args) {
+  Serial.println("got here");
+
+  String url = device.getArg(&args, 0).v;
+  String obj = device.getArg(&args, 1).v;
+
+  JsonDocument doc;
+  HTTP::makeRequest(url, &doc);
+
+  String objval = doc[obj];
+
+  Serial.println(objval);
+  device.sendArgs("http " + objval);
+  
+}
+
+/*#include "src/ArgDevice.hpp"
+
+ArgDevice device(false);
+
+void setup() {
+  device.begin(&Serial);
+
+  device.sendArgs("http https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=s8vjoJaJMQHSGvOMxgBGnwAhl1TU0FLz ticker");
+
+  device.addEventListener("http", http);
 }
 void loop() {
   device.update();
 }
 
 void http(Args args) {
+
   Serial.println("http");
-}
+  Serial.println(device.getArg(&args, 0).v);
+  
+}*/
